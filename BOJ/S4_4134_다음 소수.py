7a87b809472e4f561a598c3d25dbc633ee09e25e
남우현 <skyainq@naver.com>
2024-08-29 21:21:22 +0900

    240829(아프기 싫다~)

--- BOJ/S4_4134_다음 소수.py
+++ BOJ/S4_4134_다음 소수.py
@@ -1,16 +1,29 @@
 import sys
 
 def isPrime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
     
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    
+    return True
+
 n = int(input())
 for __ in range(n):
     k = int(sys.stdin.readline().strip())
+    if k <= 1:
+        k = 2
     while True:
         if isPrime(k):
             print(k)
             break
         k += 1
+        
